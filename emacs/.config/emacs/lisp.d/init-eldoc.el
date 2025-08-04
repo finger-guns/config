e3eadcf4dca0770abccfb1a74c7c670a9e83eeb2
samuel O'Flynn <samuelflynnmcilwain@gmail.com>
2025-08-05 07:42:56 +1000

    chore: making sure nothing has been missed

--- emacs/.config/emacs/lisp.d/init-eldoc.el
+++ emacs/.config/emacs/lisp.d/init-eldoc.el
@@ -4,15 +4,15 @@
 
 ;;; Code:
 
-(use-package eldoc-box
-  :ensure t
-  :diminish ""
-  :hook ((eldoc-mode . eldoc-box-hover-mode)
-         (eglot--managed-mode . eldoc-box-hover-at-point-mode))
-  :config
-  (setq eldoc-echo-area-use-multiline-p t)
-  (diminish 'eldoc-box-hover-mode "")
-  (diminish 'eldoc-box-hover-at-point-mode ""))
+;; (use-package eldoc-box
+;;   :ensure t
+;;   :diminish ""
+;;   :hook ((eldoc-mode . eldoc-box-hover-mode)
+;;          (eglot--managed-mode . eldoc-box-hover-at-point-mode))
+;;   :config
+;;   (setq eldoc-echo-area-use-multiline-p t)
+;;   (diminish 'eldoc-box-hover-mode "")
+;;   (diminish 'eldoc-box-hover-at-point-mode ""))
 
 (use-package eldoc
   :diminish ":edoc:"
